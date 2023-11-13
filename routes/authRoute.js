43d44cbf850ab103673264c1361e8b8f48558d58
Huuthien1404 <thie1232135@gmail.com>
2023-11-13 20:03:50 +0700

    fixed logout user

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,6 +4,6 @@ const middlewareController = require("../controllers/middlewareController");
 router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
 router.post("/refresh", authController.requestRefreshToken);
-router.post("logout", middlewareController.verifyToken, authController.logoutUser);
+router.post("/logout", middlewareController.verifyToken, authController.logoutUser);
 router.post("/logged-in", authController.loggedInUser);
 module.exports = router;
